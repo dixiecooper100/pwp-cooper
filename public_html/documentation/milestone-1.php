55b6ceaa8c930fcc0ef4b2a408f4c325076ba347
Dixie Cooper <dcooper30@cnm.edu>
2020-07-15 14:30:00 -0600

    Have the beginning of my persona finished, still working on CSS

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,6 @@
+Purpose:
+    The purpose of my PWP is for me to stay in contact with my students.  I will post assignments, challenges, info on coding opportunities, as well as give them an option to submit a message box where they can tell me what they have been up to.  I would also love to include links to some of the assignments that students I have taught have made as a reference for future employers to see what kind of things I went over in my coding class at George I. Sanchez.
+Audience:
+    My audience will be students of all ages.  I plan to post the link to this website on my social media as well as linked in pages so old students can visit my page and either message me or read about the coding opportunities and challenges I will post.
+Goal:
+    This website will not only serve as a way for me to keep in contact with my students, but will also be a place I can showcase my teaching career so far and send it to future employers to show them what kinds of things I have done with my students.
