a8dcdf58c9deced865834dd9a6ccac3bba470f05
Dixie Cooper <dcooper30@cnm.edu>
2020-08-13 15:24:08 -0600

    added contact form JS

---
+++ public_html/script.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+
+  $("#contact-form").validate({
+    debug: true,
+    errorClass: "alert alert-danger",
+    errorLabelContainer: "#output-area",
+    errorElement: "div",
+
+    //rules for contact form info
+    rules: {
+      name: {
+        required: true
+      },
+      year: {
+        required: true
+      },
+      email: {
+        email: true,
+        required: false
+      },
+      message: {
+        required: true,
+        maxlength: 2000
+      }
+    },
+
+//error messages
+    messages: {
+      name: {
+        required: "Name is a required field",
+      },
+      year: {
+        required: "This is a required field",
+      },
+      message: {
+        required: "This is a required field",
+        maxlength: "Message is too long!!"
+      },
+    },
+
+    submitHandler: function (form) {
+      $("contact-form").ajaxSubmit({
+        type: "POST",
+        url: $('#contact-form').attr('action'),
+        success: function (ajaxOutput) {
+          $("#output-area").css("display", "")
+          $("#output-area").html(ajaxOutput)
+
+          if ($(".alert-success").length >= 1) {
+            $("#contact-form")[0].reset()
+          }
+        }
+      })
+    }
+
+  })
+
+})
--- src/index.js
+++ src/index.js
@@ -1,20 +1,56 @@
 const express = require('express')
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const Recaptcha = require('express-recaptcha').RecaptchaV2
+const {check, validationResult} = require("express-validator")
+const mailgun = require('mailgun-js')
+
 
 //app variable
 const app = express()
 
 app.use(morgan('dev'))
 app.use(express.json())
+app.use(bodyParser.urlencoded({extend: false}))
+app.use(bodyParser.json())
+const recaptcha = new Recaptcha(process.env.Recaptcha_Site_Key, process.env.Recaptcha_Secret_Key)
+
+const RequestValidation = [
+  check ("name", "A valid name is required").not().isEmpty().trim().escape(),
+  check ("year", "A valid year/grade is required").not().isEmpty().trim().escape(),
+  check ("email", "A valid email is required").optional().isEmail().normalizeEmail(),
+  check("message", "A message is required to send an email").not().isEmpty().trim().escape().isLength({max:2000})
+]
 
 const indexRoute = express.Router()
 
+
 const indexRouteMiddleware = (request, response, nextFunction)  => {
   console.log ("request", request)
-  return response.json ("Is this thing on?")
+  nextFunction()
 }
 
-indexRoute.route('/').get(indexRouteMiddleware)
+const handleEmailPost = function(request, response, nextFunction) => {
+  response.appen('Content-Type', 'text/html')
+
+  if(request.recaptcha.error){
+    return response.send(`<div class='alert alert-danger' role ='alert'> <strong> There was an error with Recaptcha please try again</strong></div>`)
+  }
+
+  const errors = validationResult(request)
+
+  if(!errors.isEmpty()) {
+    const currentError = errors.array ()[0]
+  }
+
+}
+
+
+
+
+indexRoute.route('/')
+  .get(indexRouteMiddleware)
+  .post(recaptcha.middleware.verify, requestValidation, handleEmailPost)
 
 app.use('/apis', indexRoute)
 
